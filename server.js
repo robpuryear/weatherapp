5124872dd37a9d674bd895c34bbd5a5b19f48c5c
Rob Puryear <sdpuryea@us.ibm.com>
2021-11-24 14:11:10 -0500

    Adding weather code

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var PORT = 3000;
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
+var location = require("./location");
+var weather = require("./weather");
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/weather', function(req, res) {
-    res.send('You sent the name "' + req.body.city + '".');
-  });
+app.post("/weather", function (req, res) {
+  weather(req.body.city)
+    .then(function (currentWeather) {
+      console.log("Location was provided");
+      res.send(currentWeather);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  // res.send('You sent the name "' + req.body.city + '".');
+});
 
 // pulls in the public directory as the main page
 // http://localhost:3000
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.listen(PORT,function(){
-    console.log('Express server running on port ' + PORT);
+app.listen(PORT, function () {
+  console.log("Express server running on port " + PORT);
 });
